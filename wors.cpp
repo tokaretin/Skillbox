54c46b555521d1578600422e75ea55aea7f8a6c0
tokaretin <tokaretin@gmail.com>
2023-05-31 22:08:47 +0200

    srgsgh

---
+++ wors.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    // Пользователь вводит с клавиатуры число n — размер вектора, который надо заполнять.
+    std::string text = " ";
+    std::cout << "Enter the text: Hello World: ";
+    std::cin >> text;
+    std::cout << text;
+     std::cout << text;
+      std::cout << text;
+}
